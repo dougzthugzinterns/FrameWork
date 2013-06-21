f3aa944851120b080db90ece55cbf4a9092d1449
Christopher <christc5@egb-10-33-10-193.muo.muohio.edu>
2013-06-21 15:46:27 -0400

    Adding Framework for app

---
+++ FrameWorkApp/FrameWorkApp/TripSummaryScreen.designer.cs
@@ -0,0 +1,21 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace FrameWorkApp
+{
+	[Register ("TripSummaryScreen")]
+	partial class TripSummaryScreen
+	{
+		[Action ("toHome:")]
+		partial void toHome (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
