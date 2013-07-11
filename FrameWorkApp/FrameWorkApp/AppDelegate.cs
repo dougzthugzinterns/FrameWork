6ce6c031c8065c78225905da205c30ddea5d62c2
victoriamcie <unknown-user@172.25.54.145>
2013-07-10 20:02:33 -0400

    App Delegate Merge

--- FrameWorkApp/FrameWorkApp/AppDelegate.cs
+++ FrameWorkApp/FrameWorkApp/AppDelegate.cs
@@ -9,7 +9,6 @@ using MonoTouch.CoreLocation;
 
 namespace FrameWorkApp
 {
-
 	// The UIApplicationDelegate for the application. This class is responsible for launching the 
 	// User Interface of the application, as well as listening (and optionally responding) to 
 	// application events from iOS.
@@ -19,24 +18,21 @@ namespace FrameWorkApp
 		ArrayList coordList = new ArrayList ();
 		// class-level declarations
 		//MapServices.ProvideAPIKey ("AIzaSyCgSqffPgmaQlNENxSehjScWg2VLDVxJ-w");
-
 		public override UIWindow Window {
 			get;
 			set;
 		}
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options){
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
 			MapServices.ProvideAPIKey ("AIzaSyCgSqffPgmaQlNENxSehjScWg2VLDVxJ-w");
-// check for a notification
-			if (options != null)
-			{
+			// check for a notification
+			if (options != null) {
 				// check for a local notification
-				if (options.ContainsKey(UIApplication.LaunchOptionsLocalNotificationKey))
-				{
-					var localNotification = options[UIApplication.LaunchOptionsLocalNotificationKey] as UILocalNotification;
-					if (localNotification != null)
-					{
-						new UIAlertView(localNotification.AlertAction, localNotification.AlertBody, null, "OK", null).Show();
+				if (options.ContainsKey (UIApplication.LaunchOptionsLocalNotificationKey)) {
+					var localNotification = options [UIApplication.LaunchOptionsLocalNotificationKey] as UILocalNotification;
+					if (localNotification != null) {
+						new UIAlertView (localNotification.AlertAction, localNotification.AlertBody, null, "OK", null).Show ();
 						// reset our badge
 						UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
 					}
@@ -45,15 +41,14 @@ namespace FrameWorkApp
 			return true;
 		}
 
-public override void ReceivedLocalNotification(UIApplication application, UILocalNotification notification)
+		public override void ReceivedLocalNotification (UIApplication application, UILocalNotification notification)
 		{
 			// show an alert
 		
-			new UIAlertView(notification.AlertAction, notification.AlertBody, null, "OK", null).Show();
+			new UIAlertView (notification.AlertAction, notification.AlertBody, null, "OK", null).Show ();
 			
 			// reset our badge
 			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
-
 		}
 
 		// This method is invoked when the application is about to move from active to inactive state.
@@ -61,24 +56,25 @@ public override void ReceivedLocalNotification(UIApplication application, UILoca
 		public override void OnResignActivation (UIApplication application)
 		{
 		}
+
 		// This method should be used to release shared resources and it should store the application state.
 		// If your application supports background exection this method is called instead of WillTerminate
 		// when the user quits.
 		public override void DidEnterBackground (UIApplication application)
 		{
 		}
+
 		// This method is called as part of the transiton from background to active state.
 		public override void WillEnterForeground (UIApplication application)
 		{
-			if(CLLocationManager.Status == CLAuthorizationStatus.Denied){
-				new UIAlertView("Location Services must be enabled to use application!","We noticed you have disabled location services for this application. Please enable these before continuing. Please enable these before starting a new trip.", null, "OK", null).Show();
+			if (CLLocationManager.Status == CLAuthorizationStatus.Denied) {
+				new UIAlertView ("Location Services must be enabled to use application!", "We noticed you have disabled location services for this application. Please enable these before continuing. Please enable these before starting a new trip.", null, "OK", null).Show ();
 			}
 		}
+
 		// This method is called when the application is about to terminate. Save data, if needed. 
 		public override void WillTerminate (UIApplication application)
 		{
 		}
-
 	}
 }
-
