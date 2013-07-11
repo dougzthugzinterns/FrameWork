99e4b90c3611cbb240059962bf939e7efb4d7060
victoriamcie <unknown-user@172.25.54.145>
2013-07-10 20:03:27 -0400

    Add User Class File.

---
+++ FrameWorkApp/FrameWorkApp/Helper Classes/User.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace FrameWorkApp
+{
+	public class User
+	{
+		private double totalDistance = 0;
+		private int totalNumberOfEvents = 0;
+		private int totalPoints = 0;
+
+		public User (double totalDistance, int totalNumberOfEvents, int totalPoints)
+		{
+			this.totalDistance = totalDistance;
+			this.totalNumberOfEvents = totalNumberOfEvents;
+			this.totalPoints = totalPoints;
+		}
+
+		public double TotalDistance {
+			get { return totalDistance;}
+			set { this.totalDistance = value;}
+		}
+
+		public int TotalNumberOfEvents {
+			get { return totalNumberOfEvents;}
+			set { this.totalNumberOfEvents = value;}
+		}
+
+		public int TotalPoints {
+			get { return totalPoints;}
+			set { this.totalPoints = value;}
+		}
+
+		public void updateData (double tripDistance, int tripNumberOfEvents)
+		{
+			totalDistance += tripDistance;
+			totalNumberOfEvents += tripNumberOfEvents;
+
+			totalPoints = Math.Max ((totalPoints + (int)tripDistance + ((-3) * tripNumberOfEvents)), -1000000);
+		}
+	}
+}
